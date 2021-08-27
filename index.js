96037f251317720a337f2f42deacd5b6c4f5ae08
domingosl <ing.domingo.sl@gmail.com>
2021-08-27 11:03:13 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const axios = require('axios');
+
+const RestInterface = require('./rest-interface-class');
+
+const baseURLDev = "https://apidev.growish.com/v2/{{domain}}";
+const baseURLProd = "https://api.growish.com/v2/{{domain}}";
+
+
+module.exports = class ApiClient {
+
+    constructor(env, domain, baUser, baPass, options) {
+
+        this.runningRequests = 0;
+
+        this.auth = {
+            username: baUser,
+            password: baPass
+        };
+
+        this.options = options;
+
+        this.baseURL = (env === 'production' ? baseURLProd : baseURLDev).replace('{{domain}}', domain);
+
+
+        //API METHODS
+        this.Status = (new RestInterface(this, null, '/status')).setPublic({read: true});
+
+        this.Domain = (new RestInterface(this, null, '/'));
+
+        this.Users = (new RestInterface(this, '/users', '/users/{0}', '/users/{0}', '/users'));
+        this.UserWallets = (new RestInterface(this, null, null,null,'/users/{0}/wallets'));
+        this.UsersWalletsContributed = (new RestInterface(this, null, null,null,'/users/{0}/rpc-get-wallets-contributed'));
+
+
+        this.Wallets = (new RestInterface(this, '/wallets', '/wallets/{0}', '/wallets/{0}', '/wallets'));
+        this.WalletStatement = (new RestInterface(this, null, '/wallets/{0}/statement'));
+
+        this.Payins = {
+            BankWire: new RestInterface(this, '/payins/bankwire'),
+            CC: new RestInterface(this, '/payins/cc')
+        };
+
+        this.Payouts = {
+            BankWires: new RestInterface(this, '/payouts/bankwire')
+        };
+
+        this.Transactions = new RestInterface(this, null, '/transactions');
+
+        this.Transfers = new RestInterface(this, '/transfers');
+
+        this.BankAccounts = new RestInterface(this, '/bank-accounts');
+
+    };
+
+
+    async makeRequest(endpoint, method, payload, isPublic) {
+
+        const me = this;
+
+        this.runningRequests++;
+
+        try {
+
+            const url = me.baseURL + endpoint;
+
+            const axiosRequestPayload = {
+                headers: me._getHeaders(!isPublic),
+                url,
+                method,
+                data: payload,
+                timeout: 15000
+            };
+
+            if(!isPublic)
+                axiosRequestPayload.auth = this.auth;
+
+            const response = await axios(axiosRequestPayload);
+
+            this.runningRequests--;
+
+            if (!response.data || response.data.error)
+                return Promise.reject();
+
+
+            return response.data;
+        } catch (error) {
+
+            this.runningRequests--;
+
+            return Promise.reject(error.response ? error.response.data : error);
+        }
+
+    };
+
+    _getHeaders(privateHeaders = false) {
+
+        const headers = {};
+
+        headers['Content-Type'] = 'application/json';
+
+        headers['timestamp'] = (Math.floor(new Date().getTime() / 1000) - 10).toString();
+
+        return headers;
+
+    }
+
+};
---
+++ rest-interface-class.js
@@ -0,0 +1,73 @@
+module.exports = class RestInterface {
+
+    constructor(rootContext, createEndpoint, readEndpoint, editEndpoint, listEndpoint, deleteEndpoint) {
+
+        this.rootContext = rootContext;
+
+        this.isPublic = { create: false, read: false, edit: false, list: false, delete: false };
+
+        this.createEndpoint = createEndpoint;
+        this.readEndpoint = readEndpoint;
+        this.editEndpoint = editEndpoint;
+        this.listEndpoint = listEndpoint;
+    }
+
+    setPublic(options) {
+        this.isPublic = {...this.isPublic, options};
+        return this;
+    }
+
+
+    async create(payload = {}) {
+        return await this.rootContext.makeRequest(this.createEndpoint, 'POST', payload, this.isPublic.create);
+    }
+
+    async read(urlParams = [], queryParams = null) {
+        return await this.rootContext.makeRequest(prepareURL(this.readEndpoint, urlParams, queryParams), 'GET', null, this.isPublic.read);
+    }
+
+    async list(urlParams = [], queryParams = null) {
+        return await this.rootContext.makeRequest(prepareURL(this.listEndpoint, urlParams, queryParams), 'GET', null, this.isPublic.list);
+    }
+
+    async update(urlParams = [], payload = {}) {
+        return await this.rootContext.makeRequest(prepareURL(this.readEndpoint, null), 'PUT', payload, this.isPublic.update);
+    }
+
+    async delete(urlParams) {
+        return await this.rootContext.makeRequest(prepareURL(this.createEndpoint, urlParams), 'DELETE', null, this.isPublic.delete);
+    }
+};
+
+const prepareURL = (url, _urlParams, _queryParams) => {
+
+    let params;
+    if(typeof _urlParams === 'string')
+        params = [_urlParams];
+    else
+        params = _urlParams;
+
+    let res = url.replace(/{(\d+)}/g, function (match, number) {
+        return typeof params[number] !== 'undefined'
+            ? params[number]
+            : match
+            ;
+    });
+
+    if(_queryParams)
+        res += '?' + serialize(_queryParams);
+
+    return res;
+
+};
+
+const serialize = (obj) => {
+    const str = [];
+
+    for (let p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+
+    return str.join("&");
+}
