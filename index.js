9b949ed1bf6b54d03d24f3888105bc75c18aede0
domingosl <ing.domingo.sl@gmail.com>
2021-09-03 14:45:07 +0200

    Added personal wallet endpoint

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ module.exports = class ApiClient {
         this.Domain = (new RestInterface(this, null, '/'));
 
         this.Users = (new RestInterface(this, '/users', '/users/{0}', '/users/{0}', '/users'));
+        this.UserPersonalWallet = (new RestInterface(this, null, '/users/{0}/get-personal-wallet'));
         this.UserWallets = (new RestInterface(this, null, null,null,'/users/{0}/wallets'));
         this.UsersWalletsContributed = (new RestInterface(this, null, null,null,'/users/{0}/rpc-get-wallets-contributed'));
 
