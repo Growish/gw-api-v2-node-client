329228338124804c48cafe45ee65b8370f5afc72
Lorenzo Colombini <lorenzo.colombini@growishpay.com>
2025-12-19 12:34:57 +0100

    [api] new endpoint /pisp/validate/pagopa/flowpay

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ module.exports = class ApiClient {
             onboarding: new RestInterface(this, '/pisp/onboarding/{0}'),
             checkout: new RestInterface(this, '/pisp/checkout/{0}','/pisp/transactions/{0}/legalUser/{1}/checkout/{2}'),
             pagopa: new RestInterface(this, '/pisp/checkout/pagopa/flowpay'),
-            receipt: new RestInterface(this, '/pisp/transactions/receipt/flowpay')
+            receipt: new RestInterface(this, '/pisp/transactions/receipt/flowpay'),
+            validate: new RestInterface(this, '/pisp/validate/pagopa/flowpay')
         }
         this.UBODeclarations = new RestInterface(this, '/users/{0}/ubo-declarations', '/users/{0}/ubo-declarations/{1}', null, '/users/{0}/ubo-declarations');
         this.UBO = new RestInterface(this, '/users/{0}/ubo-declarations/{1}/ubos', '/users/{0}/ubo-declarations/{1}/ubos/{2}');
